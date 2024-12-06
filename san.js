75fbcacf1ee987896c0fba31422fb10429070d28
Lukigays <luki85030@gmail.com>
2024-12-07 01:30:24 +0700

    SCBOTTELE

---
+++ san.js
@@ -0,0 +1,13 @@
+const settings = {
+  token: '7904974172:AAEWc6prbAokEiRF_4ZLFt5ioKbfVA',// Token Bot panel dan domain
+  adminId: '6791166535',
+  urladmin: 'https://t.me/Nanzzxbotz',
+  domain: 'https://willstore.mypanel-ku.my.id', // Isi dengan domain yang digunakan
+  plta: 'ptla_D2wifDmipOR3ZacvKGcP9F76cTU3pMdAAe03rag5syK', // Isi dengan nilai plta yang sesuai
+  pltc: 'ptlc_7wCJMLlVBCkt7LMQQTGDF8drxnTzp6h9aa5Zo0Vz5f0', // Isi dengan nilai pltc yang sesuai
+  pp: 'https://tmpfiles.org/dl/14426067/1729079343765.jpg',
+  loc: '1', // Isi dengan lokasi yang diinginkan
+  eggs: '15'
+};
+
+module.exports = settings;
